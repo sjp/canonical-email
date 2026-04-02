e40db52c621634dd00e7d271e731f68bce755514
Simon Potter <simon@sjp.co.nz>
2026-04-03 12:02:09 +1300

    Update sass support

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +11,5 @@ export default defineConfig({
       },
     }),
   ],
+  css: { preprocessorOptions: { scss: { quietDeps: true } } },
 });
-
